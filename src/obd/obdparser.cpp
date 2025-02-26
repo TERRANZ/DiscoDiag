12e602cc40a13c697fc4fb51ce00db3ff5304c67
Vadim Korostelev <terranz@terranz.mine.nu>
2025-02-26 12:42:11 +0400

    парсер на новый лад

--- src/obd/obdparser.cpp
+++
@@ -1,2 +0,0 @@
-#include "obdparser.h"
-
--- src/obd/obdparser.h
+++ src/obd/obdparser.h
@@ -5,25 +5,15 @@
 #include <QStringList>
 
 class ObdParser {
-    //        <   hdr  >  <???> <cmd> <info>
-    //        0  1  2  3  4  5  6  7  8A 9B C  D
-    //example 18 DA F1 1A 04 62 19 6D 31 temp
-    //example 18 DA F1 1A 07 62 D1 20 01 01 00 00 rear block
-    //example 18 DA F1 2B 05 62 3B 03 00 EF suspension height
-
-    //example 18 DA F1 18 04 62 1F D1 08 - P
-    //example 18 DA F1 18 04 62 1F D1 07 - R
-    //example 18 DA F1 18 04 62 1F D1 00 - N
-    //example 18 DA F1 18 04 62 1F D1 09 - D
-    //example 18 DA F1 18 04 62 1F D1 0A - M
-
+    // <head> ecu          A
+    // 22F405 7E8 04 62 01 81
 public:
     static int extractDigitA(const QString &data) {
-        return getIntAtPos(data, 8);
+        return getIntAtPos(data, 5);
     }
 
     static int extractDigitB(const QString &data) {
-        return getIntAtPos(data, 9);
+        return getIntAtPos(data, 6);
     }
 
     static int getIntAtPos(const QString &data, const int pos) {
@@ -32,14 +22,13 @@ public:
             const QString &result = split.at(pos);
             bool ok;
             return result.toInt(&ok, 16);
-        } else {
-            return -1;
         }
+        return -1;
     }
 
     static QString extractCommandId(const QString &data) {
         const QStringList &split = data.split(QChar(' '));
-        return split.at(6) + split.at(7);
+        return split.at(0);
     }
 };
 
